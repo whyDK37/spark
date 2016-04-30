c1f964a15ecb3e4dd4e012dd7da1a2eb7ae8d73f
WangHuanyu <why.DK37@gmail.com>
2016-04-30 18:27:49 +0800

    spark test

---
+++ src/main/java/org/apache/spark/examples/streaming/StreamingExamples.java
@@ -0,0 +1,9 @@
+package org.apache.spark.examples.streaming;
+
+/**
+ * Created by whydk on 2016/4/30.
+ */
+public class StreamingExamples {
+    public static void setStreamingLogLevels() {
+    }
+}
